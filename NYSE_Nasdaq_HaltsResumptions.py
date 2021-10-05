8ff0537057a3ac4122e0fc531bcd274431d673a0
mpucci92 <42786192+mpucci92@users.noreply.github.com>
2021-10-04 22:20:00 -0400

    Add files via upload

---
+++ NYSE_Nasdaq_HaltsResumptions.py
@@ -0,0 +1,25 @@
+from datetime import date
+import pandas as pd
+
+def downloadAmericanHalts(path_to_save):
+    currentDate = date.today()
+    nyse_nasdaq_url = f'https://www.nyse.com/api/trade-halts/historical/download?symbol=&reason=&sourceExchange=&haltDateFrom={currentDate}&haltDateTo='
+    df = pd.read_csv(nyse_nasdaq_url)
+    df.to_csv(path_to_save, index=False)
+
+
+def cleanNamesAmerican(df):
+    cleanNames = []
+
+    for name in df.Name:
+        if ',' in name:
+            company_name = name.split(',')[0]
+        elif '.' in name:
+            company_name = name.split('.')[0]
+        else:
+            company_name = name
+
+        cleanNames.append(company_name)
+
+    return cleanNames
+
